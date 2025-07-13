bd6a428f6b7c15d1b59d30d4ce481e3d1934f7d2
arun100520 <155867427+arun100520@users.noreply.github.com>
2025-07-13 12:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def download():
     return send_file("static/output.mp4", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
